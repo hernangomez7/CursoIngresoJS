2157a14778398d50ac1ad80cd9d2f74cfc04c540
hernangomez7 <dath_math@hotmail.com>
2017-02-02 20:30:21 -0300

    Add files via upload

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,8 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{
+	alert("Esto funciona de maravilla");
+	//esto es un comentario de una linea
+	/*estoes un comentario bloque
+}
+
---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,18 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var nombre;
+	nombre = prompt("robot","talk to me or die");
+	alert(nombre);
+
+	
+
+
+
+
+
+	//var person = prompt("Ingresa tu nombre", "Natalia Natailia");
+
+}
+
