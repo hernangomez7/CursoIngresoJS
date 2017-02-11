985708bfe77872b186ae75c2dbd5e6a52d11353b
hernangomez7 <dath_math@hotmail.com>
2017-02-10 22:09:22 -0300

    Todos los FOR

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,27 +1,27 @@
 function Mostrar()
 {
 	var numeroUno;
-	var numero=prompt("ingrese el número de repeticiones");
 	var numeroDos;
 	var resultado;
+	var numero=prompt("ingrese el número de repeticiones");
 	var divisoresCant=0;
 	var text="";
 	var numeroDos=parseInt(numero);
 	var numero=parseInt(numero);
 	divisoresCant=parseInt(divisoresCant);
-	if(isNaN(numero))
+	/*if(isNaN(numero))
 	{
 		alert("introduce un numero");
 	}
-	numero=Math.abs(numero);
+	numero=Math.abs(numero);*/
 	for(numeroUno=2;numeroUno<numero;numeroUno++)
 	{
-		if((numero/2))
+		if(numero/numero-1)
 		{
 			text+=", "+numeroUno;
 		}	
 	}
-	divisoresCant=divisoresCant+2
+	divisoresCant=divisoresCant+1
 	alert("divisores en contrados = "+divisoresCant);
 	alert("divisores = 1"+text+", "+numero);
 }
