714fcab46a84ec544312097aee54721001713e4b
hernangomez7 <dath_math@hotmail.com>
2017-02-16 17:05:09 -0300

    ejercicio 8 for

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,27 +1,27 @@
 function Mostrar()
 {
 	var numeroUno;
-	var numeroDos;
-	var resultado;
 	var numero=prompt("ingrese el número de repeticiones");
+	var resultado;
+	var primo=0;
 	var divisoresCant=0;
 	var text="";
-	var numeroDos=parseInt(numero);
 	var numero=parseInt(numero);
 	divisoresCant=parseInt(divisoresCant);
-	/*if(isNaN(numero))
+	numero=Math.abs(numero);
+	for(numeroUno=1;numeroUno<(numero+1);numeroUno++)
 	{
-		alert("introduce un numero");
-	}
-	numero=Math.abs(numero);*/
-	for(numeroUno=2;numeroUno<numero;numeroUno++)
-	{
-		if(numero/numero-1)
+		//resultado=(numero)%numeroUno;
+		if(numero%numeroUno==0)
 		{
-			text+=", "+numeroUno;
+			primo++;
 		}	
 	}
-	divisoresCant=divisoresCant+1
-	alert("divisores en contrados = "+divisoresCant);
-	alert("divisores = 1"+text+", "+numero);
+	if (primo!=2)
+	{
+		alert("no es primo");
+	}else if (primo==0)
+	{
+		alert("es primo");
+	}
 }
