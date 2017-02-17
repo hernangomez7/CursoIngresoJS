70a48cf9b7d2dce6214a3b5b156f2acc6bc4ef86
hernangomez7 <dath_math@hotmail.com>
2017-02-16 22:08:49 -0300

    Add files via upload

--- 8-TPs/jsReflejosDaltonicos1.js
+++ 8-TPs/jsReflejosDaltonicos1.js
@@ -4,18 +4,36 @@ mostrara el texto de un color entre los 6 posibles
  para que el jugador sepa que botón tocar .Al tocar 
  el botón correcto se informara cuanto tiempo tardo.
 */
-var ColorSecreto;
-var tiempoInicio;
-function comenzar()
-{
+	var ColorSecreto;
+	var tiempoInicio;
+	var Responder;
+	var tiempoFinal;
+	function comenzar()
+	{
+		ColorSecreto=Math.floor(Math.random()*6)+1;
+		tiempoInicio=  new Date();
+		tiempoInicio=tiempoInicio.getTime();
+		console.log(ColorSecreto+" color sercreto");
+	}//FIN DE LA FUNCIÓN
 
-	
+	function Responder(azul)
+	{
 
-}//FIN DE LA FUNCIÓN
-function Responder(colorParametro)
-{
-	
-	
-
-
-}//FIN DE LA FUNCIÓN
+		tiempoFinal= new Date();
+		tiempoFinal=tiempoFinal.getTime();
+		resultado=tiempoFinal-tiempoInicio;
+		alert(resultado/1000+" segundos");
+		switch(ColorSecreto==1)
+		{
+			case 1:
+			alert("gano");
+			break;
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			alert("perdio");
+			break;
+		}
+	}//FIN DE LA FUNCIÓN
