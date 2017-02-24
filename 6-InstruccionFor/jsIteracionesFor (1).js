240f270a8fbb7e59c13072fc93762f2d0620f382
hernangomez7 <dath_math@hotmail.com>
2017-02-23 22:17:58 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,3 +1,4 @@
+/*
 function Mostrar()
 {
 	var numeroAscenente;
@@ -8,3 +9,102 @@ function Mostrar()
 	}
 	alert(text);
 }
+*/
+/*
+	function Mostrar()
+	{
+		var cont=0;
+		for(;;)
+		{
+			alert(cont);
+			cont++;
+			if(cont==5)
+			{
+				break;
+			}
+		}
+	 }
+*/
+	function Mostrar()
+	{
+		var cont=0;
+		var numeroRan=0;
+		var contador=0;
+		var contadorUno=0;
+		var contadorDos=0;
+		var contadorTres=0;
+		var contadorCuatro=0;
+		var contadorCinco=0;
+		var contadorSeis=0;
+		var contadorSiete=0;
+		var contadorOcho=0;
+		var contadorNueve=0;
+		var contadorDiez=0;
+		var porcentajeUno;
+		for(;cont<1000;cont++)
+		{
+			numeroRan=Math.floor(Math.random()*10)+1;
+			contador=contador+1;
+			switch(numeroRan)
+			{
+				case 1:
+				contadorUno=contadorUno+1;
+				break;
+				case 2:
+				contadorDos=contadorDos+1;
+				break;
+				case 3:
+				contadorTres=contadorTres+1;
+				break;
+				case 4:
+				contadorCuatro=contadorCuatro+1;
+				break;
+				case 5:
+				contadorCinco=contadorCinco+1;
+				break;
+				case 6:
+				contadorSeis=contadorSeis+1;
+				break;
+				case 7:
+				if (contadorSiete==7)
+				{
+					continue;
+				}
+				contadorSiete=contadorSiete+1;
+				break;
+				case 8:
+				contadorOcho=contadorOcho+1;
+				break;
+				case 9:
+				contadorNueve=contadorNueve+1;
+				break;
+				case 10:
+				contadorDiez=contadorDiez+1;
+				break;
+			}
+		}
+		/*
+		console.log(contadorUno+" unos");
+		console.log(contadorDos+" Dos");
+		console.log(contadorTres+" tres");
+		console.log(contadorCuatro+" cuatros");
+		console.log(contadorCinco+" cincos");
+		console.log(contadorSeis+" seis");
+		console.log(contadorSiete+" sietes");
+		console.log(contadorOcho+" ochos");
+		console.log(contadorNueve+" nueves");
+		console.log(contadorDiez+" diez");
+        */
+        console.log(contador);
+		console.log(contadorUno/contador*100+" %");
+		console.log(contadorDos/contador*100+" %");
+		console.log(contadorTres/contador*100+" %");
+		console.log(contadorCuatro/contador*100+" %");
+		console.log(contadorCinco/contador*100+" %");
+		console.log(contadorSeis/contador*100+" %");
+		console.log(contadorSiete/contador*100+" %");
+		console.log(contadorOcho/contador*100+" %");
+		console.log(contadorNueve/contador*100+" %");
+		console.log(contadorDiez/contador*100+" %");
+
+	 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,10 +1,79 @@
+	/*
 	function Mostrar()
-{
-	var numeroAscenente;
-	text="";
-	for(numeroAscenente=10;numeroAscenente>0;numeroAscenente--)
-	{
-		text+=" "+numeroAscenente;
+	{	
+		var numeroAscenente;
+		text="";
+		for(numeroAscenente=10;numeroAscenente>0;numeroAscenente--)
+		{
+			text+=" "+numeroAscenente;
+		}
+		alert(text);
 	}
-	alert(text);
-}
+	*/
+	function Mostrar()
+	{	
+		var mascota;
+		var mascotaTiene=0;
+		var mascotaNoTiene=0;
+		var tipoMascota;
+		var contador=0;
+		var edad=0;
+		var edadMayor=0;
+		var nombrePrimerGato;
+		var flagcat="vacio"
+		var sexo;
+		var edadGato=0;
+		var nombreGato;
+		for(;contador<5;)
+		{
+			edad=prompt("que edad tiene");
+			edad=parseInt(edad);
+			if (isNaN(edad))
+			{
+				edad=0;
+			}
+			mascota=prompt("tiene mascota?");
+			while(mascota!="si"&&mascota!="no")
+			{
+				alert("responda la pregunta");
+				mascota=prompt("tiene mascota");
+				if (mascota=="si")
+				{
+					mascotaTiene=mascotaTiene+1;
+					tipoMascota=prompt("ingrese tipo de mascota perro o gato");
+					while(tipoMascota!="gato"&&tipoMascota!="perro")
+					{
+						alert("responda la pregunta");
+						tipoMascota=prompt("ingrese tipo de mascota");
+					}
+					if(mascota=="gato")
+					{
+						nombreGato=prompt("nombre del  gato?")
+						edadGato=prompt("ingrese la edad del gato");
+						if(edadGato>0&&edadGato<6)
+						{
+							if(edad==30&&flagcat=="vacio")
+							{
+								nombrePrimerGato=nombreGato
+							}
+						}
+					}
+				}
+				else
+				{
+					mascotaNoTiene=mascotaNoTiene+1;
+				}
+			}
+			
+			if(edad>edadMayor&&mascota="no")
+			{
+				edadMayor=edad;
+			}
+			contador++;
+			//nombre del prime gato que sea de una mujer mayor a 30 años y el gato no tenga mas 5 años de edad
+		}
+	    
+
+
+
+	}
